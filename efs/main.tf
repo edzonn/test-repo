21024e2afc8ba7cde7262e8e5ae12cffbf673534
test <docker@DESKTOP-DHPR6IQ>
2024-02-13 19:32:04 +0800

    EFS

--- efs/main.tf
+++ efs/main.tf
@@ -1,79 +1,21 @@
+# data "aws_kms_key" "this" {
+#   key_id = "aws/elasticfilesystem"
+# }
 
-resource "aws_efs_file_system" "file_system" {
-  creation_token                  = var.creation_token
-  encrypted                       = var.encrypted
-  kms_key_id                      = var.encrypted && var.kms_key_id != null ? var.kms_key_id : null
-  performance_mode                = var.performance_mode
-  throughput_mode                 = var.throughput_mode
-  provisioned_throughput_in_mibps = var.throughput_mode == "provisioned" ? var.provisioned_throughput_in_mibps : null
-  tags                            = var.tags
-  lifecycle_policy {
-    transition_to_ia = var.lifecycle_policy
+data "terraform_remote_state" "module_outputs" {
+  backend = "s3"
+  config = {
+    bucket = "aws-terraform-tfstatefile-001"
+    key    = "dev/terraform.statefile"
+    region = "ap-southeast-1"
   }
 }
 
-resource "aws_efs_mount_target" "alpha" {
-  file_system_id = aws_efs_file_system.file_system.id
-  subnet_id      = var.subnet_id
+module "efs" {
+  source            = "/mnt/c/Users/user/Desktop/terraform/test-repo/modules/efs"
+  efs_name          = "my-efs"
+  subnet_ids        = data.terraform_remote_state.module_outputs.outputs.private_subnet_ids
+  security_group_id = "sg-0a84e76479639d970"
+  kms_key_id        = "arn:aws:kms:ap-southeast-1:092744370500:key/7f78820d-cd44-4ba1-9523-76f7a65673f8"
+  vpc_cidr         = data.terraform_remote_state.module_outputs.outputs.vpc_id
 }
-
-# data "availability_zones" "available" {}
-
-# resource "aws_efs_file_system" "efs" {
-#   creation_token   = "efs"
-#   performance_mode = "generalPurpose"
-#   throughput_mode  = "bursting"
-#   encrypted        = "true"
-#   tags = {
-#     Name = "EFS"
-#   }
-# }
-
-
-# resource "aws_efs_mount_target" "efs-mt" {
-#   #    count = length(data.aws_availability_zones.available.names)
-#   file_system_id  = aws_efs_file_system.efs.id
-#   subnet_id       = data.terraform_remote_state.module_outputs.outputs.private_subnet_ids.count > 0 ? data.terraform_remote_state.module_outputs.outputs.private_subnet_ids[count.index] : data.terraform_remote_state.module_outputs.outputs.public_subnet_ids[count.index]
-#   security_groups = data.terraform_remote_state.module_outputs.outputs.security_group_ids
-# }
-
-# resource "aws_security_group" "ec2" {
-#   name        = "allow_efs"
-#   description = "Allow efs outbound traffic"
-#   vpc_id      = data.terraform_remote_state.module_outputs.outputs.vpc_id
-#   ingress {
-#     cidr_blocks = ["0.0.0.0/0"]
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#   }
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "allow_efs"
-#   }
-# }
-
-# resource "aws_security_group" "efs" {
-#   name        = "efs-sg"
-#   description = "Allos inbound efs traffic from ec2"
-#   vpc_id      = data.terraform_remote_state.module_outputs.outputs.vpc_id
-
-#   ingress {
-#     security_groups = [aws_security_group.ec2.id]
-#     from_port       = 2049
-#     to_port         = 2049
-#     protocol        = "tcp"
-#   }
-
-#   egress {
-#     security_groups = [aws_security_group.ec2.id]
-#     from_port       = 0
-#     to_port         = 0
-#     protocol        = "-1"
-#   }
-# }
